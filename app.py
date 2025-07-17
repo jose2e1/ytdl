49222252b3a63eaa5c82a7f8449e67baf1880aa8
jose2e1 <139660234+jose2e1@users.noreply.github.com>
2025-07-17 22:10:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,8 @@ def download():
 
     return send_file(filename, as_attachment=True)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
